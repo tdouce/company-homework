9c9da24180a400042e201d148faa0d20d7a174b5
Travis Douce <travisdouce@gmail.com>
2020-10-10 11:26:59 -0400

    Add ability to indicate that an order is a gift
    This adds the ability for the user to indicate that an order is a gift. Additionallly, it allows the user to add a message for the gift.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new(product: Product.find(params[:product_id]))
+    @is_gift = params[:gift].present?
   end
 
   def create
--- spec/features/purchase_product_spec.rb
+++ spec/features/purchase_product_spec.rb
@@ -1,72 +1,130 @@
 require 'rails_helper'
 
 RSpec.feature "Purchase Product", type: :feature do
-  scenario "Creates an order and charges us" do
-    product = Product.create!(
-      name: "product1",
-      description: "description2",
-      price_cents: 1000,
-      age_low_weeks: 0,
-      age_high_weeks: 12,
-    )
+  context "When the order is a gift" do
+    scenario "Creates an order as a gift and charges us" do
+      product = Product.create!(
+        name: "product1",
+        description: "description2",
+        price_cents: 1000,
+        age_low_weeks: 0,
+        age_high_weeks: 12
+      )
 
-    visit "/"
+      parent_name = "Pat Jones"
+      child_name = "Kim Jones"
+      child_birthdate = "2019-03-03"
 
-    within ".products-list .product" do
-      click_on "More Details…"
+      child = Child.create(
+        full_name: child_name,
+        birthdate: child_birthdate,
+        parent_name: parent_name
+      )
+
+      order = Order.create(
+        user_facing_id: "064b2adb",
+        product_id: product.id,
+        child_id: child.id,
+        shipping_name: parent_name,
+        address: "123 Any St",
+        zipcode: "83701",
+        paid: true
+      )
+
+      visit "/"
+
+      within ".products-list .product" do
+        click_on "More Details…"
+      end
+
+      check("Is this a gift?")
+      click_on "Buy Now $10.00"
+
+      fill_in "order[credit_card_number]", with: "4111111111111111"
+      fill_in "order[expiration_month]", with: 12
+      fill_in "order[expiration_year]", with: 25
+      fill_in "order[shipping_name]", with: "Pat Jones"
+      fill_in "order[address]", with: "123 Any St"
+      fill_in "order[zipcode]", with: 83701
+      fill_in "order[child_full_name]", with: child_name
+      fill_in "order[child_birthdate]", with: child_birthdate
+      fill_in "order[parent_name]", with: parent_name
+      fill_in "order[message]", with: "Happy Birthday!"
+
+      click_on "Purchase"
+
+      expect(page).to have_content("Thanks for Your Order")
+      expect(page).to have_content(Order.last.user_facing_id)
+      expect(page).to have_content(child_name)
     end
-
-    click_on "Buy Now $10.00"
-
-    fill_in "order[credit_card_number]", with: "4111111111111111"
-    fill_in "order[expiration_month]", with: 12
-    fill_in "order[expiration_year]", with: 25
-    fill_in "order[shipping_name]", with: "Pat Jones"
-    fill_in "order[address]", with: "123 Any St"
-    fill_in "order[zipcode]", with: 83701
-    fill_in "order[child_full_name]", with: "Kim Jones"
-    fill_in "order[child_birthdate]", with: "2019-03-03"
-
-    click_on "Purchase"
-
-    expect(page).to have_content("Thanks for Your Order")
-    expect(page).to have_content(Order.last.user_facing_id)
-    expect(page).to have_content("Kim Jones")
-
-
   end
 
-  scenario "Tells us when there was a problem charging our card" do
-    product = Product.create!(
-      name: "product1",
-      description: "description2",
-      price_cents: 1000,
-      age_low_weeks: 0,
-      age_high_weeks: 12,
-    )
+  context "When the order is not a gift" do
+    scenario "Creates an order and charges us" do
+      product = Product.create!(
+        name: "product1",
+        description: "description2",
+        price_cents: 1000,
+        age_low_weeks: 0,
+        age_high_weeks: 12,
+        )
 
-    visit "/"
+      visit "/"
 
-    within ".products-list .product" do
-      click_on "More Details…"
+      within ".products-list .product" do
+        click_on "More Details…"
+      end
+
+      click_on "Buy Now $10.00"
+
+      fill_in "order[credit_card_number]", with: "4111111111111111"
+      fill_in "order[expiration_month]", with: 12
+      fill_in "order[expiration_year]", with: 25
+      fill_in "order[shipping_name]", with: "Pat Jones"
+      fill_in "order[address]", with: "123 Any St"
+      fill_in "order[zipcode]", with: 83701
+      fill_in "order[child_full_name]", with: "Kim Jones"
+      fill_in "order[child_birthdate]", with: "2019-03-03"
+
+      click_on "Purchase"
+
+      expect(page).to have_content("Thanks for Your Order")
+      expect(page).to have_content(Order.last.user_facing_id)
+      expect(page).to have_content("Kim Jones")
     end
 
-    click_on "Buy Now $10.00"
+    scenario "Tells us when there was a problem charging our card" do
+      product = Product.create!(
+        name: "product1",
+        description: "description2",
+        price_cents: 1000,
+        age_low_weeks: 0,
+        age_high_weeks: 12,
+        )
 
-    fill_in "order[credit_card_number]", with: "4242424242424242"
-    fill_in "order[expiration_month]", with: 12
-    fill_in "order[expiration_year]", with: 25
-    fill_in "order[shipping_name]", with: "Pat Jones"
-    fill_in "order[address]", with: "123 Any St"
-    fill_in "order[zipcode]", with: 83701
-    fill_in "order[child_full_name]", with: "Kim Jones"
-    fill_in "order[child_birthdate]", with: "2019-03-03"
+      visit "/"
 
-    click_on "Purchase"
+      within ".products-list .product" do
+        click_on "More Details…"
+      end
 
-    expect(page).not_to have_content("Thanks for Your Order")
-    expect(page).to have_content("Problem with your order")
-    expect(page).to have_content(Order.last.user_facing_id)
-    expect(page).to have_content("Kim Jones")
+      click_on "Buy Now $10.00"
+
+      fill_in "order[credit_card_number]", with: "4242424242424242"
+      fill_in "order[expiration_month]", with: 12
+      fill_in "order[expiration_year]", with: 25
+      fill_in "order[shipping_name]", with: "Pat Jones"
+      fill_in "order[address]", with: "123 Any St"
+      fill_in "order[zipcode]", with: 83701
+      fill_in "order[child_full_name]", with: "Kim Jones"
+      fill_in "order[child_birthdate]", with: "2019-03-03"
+
+      click_on "Purchase"
+
+      expect(page).not_to have_content("Thanks for Your Order")
+      expect(page).to have_content("Problem with your order")
+      expect(page).to have_content(Order.last.user_facing_id)
+      expect(page).to have_content("Kim Jones")
+    end
   end
 end
